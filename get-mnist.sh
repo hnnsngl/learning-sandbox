731837eae6e2f5792a96cb0edfb26ce80e7d6bab
Hannes Nagel <hannes.nagel@itp.uni-leipzig.de>
2016-01-09 17:59:06 +0100

    add script to fetch and unzip MNIST dataset

---
+++ get-mnist.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+# download and unzip MNIST dataset
+
+oldpwd=$(pwd)
+
+mkdir MNIST ; cd MNIST
+
+wget http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
+wget http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
+wget http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
+wget http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
+
+gunzip t10k*.gz train*.gz
+
+cd $oldpwd
